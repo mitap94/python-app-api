7181f194a00229f58172a50f28cc4716f670a640
Dimitrije Petrovic <petrovic.m.dimitrije@gmail.com>
2020-06-29 11:35:27 +0200

    Update admin to support custom user model

---
+++ app/tests/test_admin.py
@@ -0,0 +1,43 @@
+from django.test import TestCase, Client  # helper Client - force login
+from django.contrib.auth import get_user_model
+from django.urls import reverse  # generate URLs
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@gmail.com",
+            password="test123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@gmail.com",
+            password="test123",
+            name="Test user"
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)  # HTTP GET
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        # /admin/core/user/
+
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
