04eb2509217d338848c5b1c57cba3f3b6729d1f9
Dimitrije Petrovic <petrovic.m.dimitrije@gmail.com>
2020-07-12 15:47:04 +0200

    Added list recipe feature

--- app/core/models.py
+++ app/core/models.py
@@ -7,9 +7,9 @@ from django.conf import settings
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create and save new user"""
+        """Creates and saves a new user"""
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -68,7 +68,7 @@ class Recipe(models.Model):
     """Recipe object"""
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE
     )
     title = models.CharField(max_length=255)
     time_minutes = models.IntegerField()
